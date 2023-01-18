4b6474ac85f0c23554570b6db17b2723d0c3cd1a
Serj <raysan.024@gmail.com>
2023-01-18 19:28:07 +0700

    5

--- src/components/Photos.js
+++ src/components/Photos.js
@@ -3,10 +3,10 @@ import React, { useState } from 'react';
 function PhotoGallery() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [photos] = useState([
-      { url: "./img/one.jpg", caption: "Photo 1" },
-      { url: "./img/two.jpg", caption: "Photo 2" },
-      { url: "./img/three.jpg", caption: "Photo 3" },
-      { url: "./img/logo-og.png", caption: "Photo 4" }
+      { url: "http://sun9-78.userapi.com/impg/nbUKhYxc685xhKUpLC9FJbNkGZQHZWXwzLRnkQ/d-8SGjNCY4E.jpg?size=479x600&quality=96&sign=fa37f8b91a2a41408fccef2d4d7c9df4&type=album", caption: "Photo 1" },
+      { url: "https://58.img.avito.st/image/1/1.e6hNYra610F7yxVENx9s8aTB0UX5Qd-D_MHTSfHJ1Q.sTduRpshazSjpEODWtryh30hgbe-skNdDpRxIe24p9s", caption: "Photo 2" },
+      { url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRooWOAXFSq2DrYX7FBxwCPIhfN9NWPDqQOow&usqp=CAU", caption: "Photo 3" },
+      { url: "https://anime-fans.ru/wp-content/uploads/2021/05/Topovye-anime-avatarki-dlya-malchikov-skachat_24.jpg", caption: "Photo 4" }
   ]);
 
   function previousPhoto() {
@@ -27,10 +27,10 @@ function PhotoGallery() {
 
   return (
       <div>
-          <img src={currentPhoto.url} alt={currentPhoto.caption} />
+          <img className='photo' src={currentPhoto.url} alt={currentPhoto.caption} />
           <p>{currentPhoto.caption}</p>
-          <button onClick={previousPhoto}>Previous</button>
-          <button onClick={nextPhoto}>Next</button>
+          <button onClick={previousPhoto}>Назад</button>
+          <button onClick={nextPhoto}>Вперёд</button>
       </div>
   );
 }
